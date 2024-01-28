17daed034a4440f319f5947f9fd873b86c1cabd5
Elvis Rivera <therealelvisrivera@gmail.com>
2024-01-28 14:59:23 -0600

    Psedocode

--- index.js
+++ index.js
@@ -19,12 +19,20 @@ app.get('/api', (req, res) => {
 });
 
 // get all pets from the database
+//create route to handle GET request for all pets
+//connect to the data base
+//return pets as JSON response
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
     res.json(pets);
 });
 
 // get pet by owner with query string
+//create route to handle GET for request by owner name
+//extract owner name from request params
+//connect to database
+//reuturn list of pets as JSON
+//case handle for no owner for pets 
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
     const { owner } = req.query;
@@ -37,6 +45,12 @@ app.get('/api/v1/pets/owner', (req, res) => {
 });
 
 // get pet by name
+//create route to handle GET request for get pet by name
+//Extract pet name from request params
+//connect to Database
+//query to retrieve pet with specified name
+//return pet as JSON
+//case handle for pet not found
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
     const { name } = req.params;
